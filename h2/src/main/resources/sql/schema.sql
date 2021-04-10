c09499bdf7a2661215f48bb89652a743a2bc40e0
Ajay Singh <singhajay9009@gmail.com>
2021-04-10 20:30:03 +0530

    Add schema.sql

---
+++ h2/src/main/resources/sql/schema.sql
@@ -0,0 +1,8 @@
+create table student (
+    id int NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    hobby VARCHAR(100)
+    age int NOT NULL,
+    CONSTRAINT id_pk PRIMARY KEY(id)
+);
