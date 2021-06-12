28145a89cbf31eb031c1a9a5e9fa34b2f2d2e2c8
Ajay Singh <singhajay9009@gmail.com>
2021-06-12 20:54:31 +0530

    Add CustomGenerator
    Create a class implementing IdentifierGenerator

--- spring_jpa/src/main/java/com/ajay/spring/jpa/demo/entity/Employee.java
+++ spring_jpa/src/main/java/com/ajay/spring/jpa/demo/entity/Employee.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -23,8 +24,19 @@ This class is created to test ID generation strategies
 @AllArgsConstructor
 public class Employee {
 
+    //@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    //private int id;
+
+
+//    @Id
+//    @TableGenerator(name="emp_gen",table = "id_gen", pkColumnName = "gen_name", pkColumnValue = "next_val", allocationSize=100)
+//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "emp_gen")
+//    private int id;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GenericGenerator(name = "generic_gen", strategy = "com.ajay.spring.jpa.demo.entity.CustomRandomGenerator")
+    @GeneratedValue(generator= "generic_gen")
     private int id;
 
     private String name;
--- spring_jpa/src/main/resources/data.sql
+++ spring_jpa/src/main/resources/data.sql
@@ -9,7 +9,21 @@
 
 Select * from product;
 
+-- Create this table to demonstrate id generation strategy : IDENTITY
 --create table employee_id (
 --    id SERIAL PRIMARY KEY,
 --    name VARCHAR (10) NOT NULL
+--);
+
+-- drop table employee_id;
+
+-- this table to demonstrate id generation strategy : TABLE  ( does not work for postgres )
+--create table employee_id (
+--    id int PRIMARY KEY,
+--    name VARCHAR (10)
+--);
+--
+--create table id_gen (
+--    gen_name VARCHAR(50) PRIMARY KEY,
+--    next_val int   // this column name must be chosen for postgres
 --);
